860bade8f0ede3f4e732ae35eb80434fd6f0bddc
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2013-08-15 00:29:17 -0300

    Frakking Heroku is again pointing to errors that don't happen locally

--- web.rb
+++ web.rb
@@ -1,17 +1,13 @@
 require 'sinatra'
 require_relative 'article'
 
-get '/:name/' do
+get '/:name/?' do
 	article = Article.new params[:name]
 	attrs = article.attrs.clone;
 	attrs[:intro] = markdown article.intro
 	attrs[:content] = markdown article.content
 	attrs[:random_articles] = Article.list.sample 4
 	erb :article, :locals => attrs
-end
-
-get '/:name' do
-	redirect to("/#{params[:name]}/")
 end
 
 get '/:name/*' do
