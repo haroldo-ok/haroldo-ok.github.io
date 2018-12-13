0b6bab01685b4ab90a87857d482e90ef34dd6507
haroldo <haroldoop@gmail.com>
2018-12-13 21:53:00 -0200

    Only prefetch articles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 (function(){
    window.addEventListener('load', () => {
+      let rx = /^\/[\w-]+\/?$/; 
       let links = [...document.getElementsByTagName('a')].filter(a => {
-         return a.hostname === window.location.hostname;
+         return a.hostname === window.location.hostname && rx.test(a.pathname);
       });
       quicklink({urls:links});
    });
